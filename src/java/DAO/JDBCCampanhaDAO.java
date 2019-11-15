f8703f48b94f860d17deb4064d98b2d97b041443
alkarramax <alkarramax3@gmail.com>
2019-11-15 12:04:05 -0200

    NecessidadeDAO modify

--- src/java/DAO/JDBCCampanhaDAO.java
+++ src/java/DAO/JDBCCampanhaDAO.java
@@ -23,16 +23,18 @@ public class JDBCCampanhaDAO{
    
     public void inserir(Campanha campanha) {
         try {
-            String SQL = "insert into campanha(nome, descricao, local, data, imagem, aberta) "
-                    + "values (?,?,?,?,?,?)";
+            String SQL = "insert into campanha(id_necessidade1, id_necessidade2, nome, descricao, local, data, imagem, aberta) "
+                    + "values (?,?,?,?,?,?,?)";
             PreparedStatement ps = connection.prepareStatement(SQL);
-            ps.setString(1, campanha.getNome());
-            ps.setString(2, campanha.getDescricao());
-            ps.setString(3, campanha.getLocal());  
-            ps.setString(4, String.valueOf(campanha.getData()));
+            ps.setInt(1, campanha.getId_necessidade1());
+            ps.setInt(2, campanha.getId_necessidade2());
+            ps.setString(3, campanha.getNome());
+            ps.setString(4, campanha.getDescricao());
+            ps.setString(5, campanha.getLocal());  
+            ps.setString(6, String.valueOf(campanha.getData()));
             campanha.setAberta(true);
-            ps.setString(5, campanha.getImagem());
-            ps.setBoolean(6, campanha.isAberta());
+            ps.setString(7, campanha.getImagem());
+            ps.setBoolean(8, campanha.isAberta());
             ps.executeUpdate();
             ps.close();
            
@@ -107,9 +109,4 @@ public class JDBCCampanhaDAO{
             throw new RuntimeException("Erro ao buscar a campanha!", ex);
         }
     }
-
-    public void editar(Campanha campanha) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-   
 }
